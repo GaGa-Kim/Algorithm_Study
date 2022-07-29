95c23ed4900dd70254575c4b18bc588cb579f5fd
cutehumanS2 <60508828+cutehumanS2@users.noreply.github.com>
2022-07-29 12:02:55 +0900

    가희 - 구현 백준

--- 02_구현/가희/백준/팰린드롬 만들기.py
+++ 02_구현/가희/백준/팰린드롬 만들기.py
@@ -53,7 +53,7 @@ alphabet=''
 for i in range(26):
     if arr_cnt[i]%2 == 1: #저장된 갯수가 홀수이면
         odd+=1 
-        odd_alphabet+= chr(i+65) #홀수만 모아놓은 배열에wjwkd
+        odd_alphabet+= chr(i+65) #홀수만 모아놓은 배열에 저장
     alphabet+=chr(i+65)*(arr_cnt[i]//2)
 
 if odd>1: #만약 홀수가 두개 이상이면 팰린드롬 못만듦 (가운데에 하나만 있어야 하므로)
