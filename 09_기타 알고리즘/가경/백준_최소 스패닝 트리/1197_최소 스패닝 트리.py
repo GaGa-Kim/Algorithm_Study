631259be637d8f5d8306691db33688c25e7c6cf2
GaGa-Kim <wn8925@sookmyung.ac.kr>
2022-10-19 12:40:17 +0900

    가경 - 기타 알고리즘 (최소 스패닝 트리) 백준

---
+++ 09_기타 알고리즘/가경/백준_최소 스패닝 트리/1197_최소 스패닝 트리.py
@@ -0,0 +1,42 @@
+import sys
+input = sys.stdin.readline
+
+# 특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+v, e = map(int, input().split())
+
+parent = [0] * (v + 1)
+for i in range(1, v + 1):
+    parent[i] = i
+
+# 모든 간선을 담을 리스트와 최종 비용을 담을 변수
+edges = []
+result = 0
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    edges.append((c, a, b))
+
+# 간선을 비용순으로 오름차순 정렬
+edges.sort()
+
+for cost, a, b in edges:
+    # 사이클이 발생하지 않을 경우 (같은 집합이 아니라면)
+    if find_parent(parent, a) != find_parent(parent, b):
+        # 해당 간선을 최소 신장 트리 집합에 포함
+        union_parent(parent, a, b)
+        result += cost
+
+print(result)
---
+++ 09_기타 알고리즘/가경/백준_최소 스패닝 트리/1647_도시 분할 계획.py
@@ -0,0 +1,43 @@
+import sys
+input = sys.stdin.readline
+
+# 특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n, m = map(int, input().split())
+
+parent = [0] * (n + 1)
+for i in range(1, n + 1):
+    parent[i] = i
+
+# 모든 간선을 담을 리스트와 최종 비용을 담을 변수
+edges = []
+result = []
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    edges.append((c, a, b))
+
+# 간선을 비용순으로 오름차순 정렬
+edges.sort()
+
+for cost, a, b in edges:
+    # 사이클이 발생하지 않을 경우 (같은 집합이 아니라면)
+    if find_parent(parent, a) != find_parent(parent, b):
+        # 해당 간선을 최소 신장 트리 집합에 포함
+        union_parent(parent, a, b)
+        result.append(cost)
+
+# 마을을 두 개로 분리하기 위해 마지막 간선 제외하고 최소 비용 구하기
+print(sum(result[:-1]))
