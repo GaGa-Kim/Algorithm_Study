4c21be8f663c8cbdaa8d94c871565a0ffcf88bc6
GaGa-Kim <wn8925@sookmyung.ac.kr>
2022-11-07 11:16:59 +0900

    가경 - 그리디 백준

---
+++ 01_그리디/가경/백준/1758_알바생 강호.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+guest = [int(input()) for _ in range(n)]
+# 강호에게 원래 주려고 생각했던 돈이 많은 손님부터 먼저 커피를 줌
+guest = sorted(guest, reverse=True)
+
+answer = 0
+for i in range(n):
+    money = guest[i] - ((i + 1) - 1)
+    # 팁 값이 음수라면 팁을 받지 않음
+    if (money < 0):
+        continue
+    answer += money
+print(answer)
